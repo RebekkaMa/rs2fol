68eac401be8b9f80e14596a66c82bba3034bd2fc
RebekkaMa <rebekka@martens-sachsen.de>
2023-05-02 22:10:20 +0200

    First commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,59 +1,57 @@
 import PrefixParser.prefix
 import PrefixParser.uri
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.file
 import com.github.h0tk3y.betterParse.combinators.map
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.grammar.tryParseToEnd
+import com.github.h0tk3y.betterParse.parser.parseToEnd
 import com.github.h0tk3y.betterParse.parser.toParsedOrThrow
+import java.io.File
 import javax.swing.text.html.parser.Parser
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Hello : CliktCommand() {
+    val path by option(help = "The path to the RDF Surface Graph").file().prompt("The path of your RDF Surface Graph")
+    override fun run() {
 
-    val prefStr = "@prefix log: <http://www.w3.org/2000/10/swap/log#>.\n" +
-            "@prefix : <http://example.org/ns#>." +
-            "\n"
+        val prefMap = mutableMapOf<String, String>()
+        var graph = ""
 
-    val prefMap = PrefixParser.parseToEnd(prefStr)
-
-    var str = ":beetle a :Car.\n" +
-            "\n" + ":bob a :Person.\n" +
-            "\n" +
-            ":grannysmith a :Apple .\n" +
-            "\n" +
-            ":grannysmith :is :green .\n" +
-            "\n" +
-            ":mars a :Planet.\n" +
-            "\n" +
-            "(_:A) log:onNegativeSurface {\n" +
-            "    _:A :is :green.\n" +
-            "    () log:onNegativeSurface {\n" +
-            "        _:A :is :beautiful.\n" +
-            "    }.\n" +
-            "}.\n" +
-            "\n" +
-            "(_:A) log:onNegativeSurface {\n" +
-            "    () log:onNegativeSurface {\n" +
-            "        _:A :is :green.\n" +
-            "    }.\n" +
-            "    () log:onNegativeSurface {\n" +
-            "        _:A :is :beautiful.\n" +
-            "    }.\n" +
-            "}."
-
-
-
-    prefMap.forEach { (prefix, uri) ->
-        str = str.replace(regex = Regex("(^\\A|\\s)$prefix(\\w+)")) { matchResult: MatchResult ->
-            " " + uri.dropLast(1) + matchResult.groupValues.last() + ">"
+        path.bufferedReader().lines().use {lines ->
+            for (it in lines){
+                when {
+                    it.startsWith("@prefix ") ->  prefMap.putAll(PrefixParser.parseToEnd(it))
+                    it.contains("query") -> break
+                    else -> graph = graph + "\n" + it
+                }
+            }
         }
+
+        prefMap.forEach { (prefix, uri) ->
+            graph = graph.replace(regex = Regex("(^\\A|\\s)$prefix(\\w+)")) { matchResult: MatchResult ->
+                " " + uri.dropLast(1) + matchResult.groupValues.last() + ">"
+            }
+        }
+
+        val str = graph.replace(" a ", " <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ")
+
+        println(str)
+
+        var res2 = N3Parser.parseToEnd(str)
+
+//    res2 = res2?.replace(regex = Regex("'<(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>'")) {
+//        it.value.drop(2).dropLast(2).replace(regex = Regex("\\W"), "_")
+//    }
+
+        println("Result--------------------")
+        println(res2)
+        println("--------------------------")
+
+
     }
-    str = str.replace(" a ", " <http://www.w3.org/TR/rdf-schema/> ")
-
-    println(str)
-
-
-    val res2 = N3Parser.parseToEnd(str)
-
-    println(res2)
 
 }
+
+fun main(args: Array<String>) = Hello().main(args)
--- src/main/kotlin/NTPParser.kt
+++ src/main/kotlin/NTPParser.kt
@@ -16,30 +16,33 @@ object N3Parser : Grammar<String?>() {
     val space by regexToken("\\s*", ignore = true)
     val alspace by regexToken("\\s+", ignore = true)
     val dot by literalToken(".")
+    val semicolon by literalToken(";")
+    val comma by literalToken(",")
 
     val negativeSurfaceText by literalToken("<http://www.w3.org/2000/10/swap/log#onNegativeSurface>")
     val positiveSurfaceText by literalToken("<http://www.w3.org/2000/10/swap/log#onPositiveSurface>")
 
-    val variable by regexToken("_:\\w+\\b")
+    val variable by regexToken("_:\\w+")
     val v by variable use { this.text.drop(2) }
+
+    val emptyvarList by (lpar and rpar) use { "" }
+    val variableList by emptyvarList or (-lpar and oneOrMore(v and (0..1 times space)) and -rpar).map { it.joinToString(separator = ",") { tuple2 -> tuple2.t1 } }
 
     val ressource by regexToken("^<(https?|http|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>")
     val r by ressource use { "'" + this.text + "'" }
 
     val triple by (v or r) and -simpleSpace and (v or r) and -simpleSpace and (v or r) use { "triple(" + this.t1 + ", " + this.t2 + ", " + this.t3 + ")" }
-    val emptyvarList by (lpar and rpar) use { "" }
-    val variableList by emptyvarList or (-lpar and oneOrMore(v and (0..1 times space)) and -rpar).map { it.joinToString(separator = ",") { tuple2 -> tuple2.t1 } }
 
     val aParser: Parser<String> by
     oneOrMore(
         (variableList and
                 -zeroOrMore(v and (0..1 times space)) and
                 (negativeSurfaceText or positiveSurfaceText) and
-                -(0..1 times space) and
+                -optional(alspace) and
                 -lparcurl and
-                -(0..1 times space) and
-                parser(this::aParser) and
-                -(0..1 times space) and
+                -optional(alspace) and
+                (parser(this::aParser) or optional(alspace).use {"\$true"}) and
+                -optional(alspace) and
                 -rparcurl) map { (variableList, surface, rest) ->
             val variableListNotNull = variableList.isNotEmpty()
             var outputString = ""
@@ -49,7 +52,7 @@ object N3Parser : Grammar<String?>() {
                 }
             } else {
                 if (variableListNotNull) {
-                    outputString = "! [$variableList] :"
+                    outputString = "! [$variableList] : "
                 }
                 outputString = "$outputString~"
 
@@ -58,7 +61,7 @@ object N3Parser : Grammar<String?>() {
             outputString
         } or
                 triple
-                and -dot) use { this.joinToString(prefix = "(", postfix = ")", separator = " & ") }
+                and -optional(dot)) use { this.joinToString(prefix = "(", postfix = ")", separator = " & ") }
 
     override val rootParser: Parser<String> by aParser use {"fof(test,axiom,$this)." }
 }
