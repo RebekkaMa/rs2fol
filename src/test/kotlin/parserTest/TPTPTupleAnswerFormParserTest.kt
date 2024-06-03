1dbbb472d990fe4609862249a3ddf50dc3b22e98
RebekkaMa <rebekka@martens-sachsen.de>
2024-06-03 12:31:34 +0200

    Fix TPTPTupleAnswerFormParserTest

--- src/test/kotlin/parserTest/TPTPTupleAnswerFormParserTest.kt
+++ src/test/kotlin/parserTest/TPTPTupleAnswerFormParserTest.kt
@@ -4,6 +4,7 @@ import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.equality.shouldNotBeEqualUsingFields
+import io.kotest.matchers.equals.shouldBeEqual
 import parser.TptpTupleAnswerFormTransformer
 import model.rdf_term.*
 import model.rdf_term.Collection
@@ -14,7 +15,7 @@ class TPTPTupleAnswerFormParserTest : ShouldSpec(
         should("parse basic example without exception") {
             val str =
                 "[(['http://example.org/ns#beetle','http://example.org/ns#nice']|['http://example.org/ns#beetle','http://example.org/ns#green']),['http://example.org/ns#beetle','http://example.org/ns#beautiful']|_]"
-            parser.TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(
                 listOf(listOf(IRI.from("http://example.org/ns#beetle"), IRI.from("http://example.org/ns#beautiful"))),
                 listOf(
                     listOf(
@@ -28,7 +29,7 @@ class TPTPTupleAnswerFormParserTest : ShouldSpec(
         should("parse example with lists without exception") {
             val str =
                 "[[list('http://example.org/ns#s',list('http://example.org/ns#s',list('http://example.org/ns#s','\"0\"^^http://www.w3.org/2001/XMLSchema#integer'))),list]|_]"
-            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(
                 listOf(
                     listOf(
                         Collection(
@@ -60,7 +61,7 @@ class TPTPTupleAnswerFormParserTest : ShouldSpec(
         should("parse example with rdf literals without exception") {
             val str =
                 "[['\"0\"^^http://www.w3.org/2001/XMLSchema#string','\"0\"@en'],['http://example.org/ns#s', sK5]|_]"
-            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(
                 listOf(
                     listOf(
                         DefaultLiteral.fromNonNumericLiteral(
@@ -84,7 +85,7 @@ class TPTPTupleAnswerFormParserTest : ShouldSpec(
 
         should("parse example with skolem function without exception") {
             val str = "[[sK1('http://example.org/ns#b','http://example.org/ns#c')]|_]"
-            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(
                 listOf(
                     listOf(
                         BlankNode(
@@ -105,13 +106,13 @@ class TPTPTupleAnswerFormParserTest : ShouldSpec(
 
         should("parse empty result") {
             val str = "[|_]"
-            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(listOf(), listOf())
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(listOf(), listOf())
         }
 
         should("parse another basic example without exception") {
             val str =
                 "[['http://example.org/ns#beetle','\"RDF/XML Syntax Specification (Revised)\"^^http://www.w3.org/2001/XMLSchema#string'],[list('http://example.org/ns#s'),'\"That Seventies Show\"@en'],['http://example.org/ns#beetle','\"http://www.w3.org/2001/XMLSchema#string\"^^http://www.w3.org/2001/XMLSchema#string']|_]"
-            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldNotBeEqualUsingFields Pair(
+            TptpTupleAnswerFormTransformer.parseToEnd(str) shouldBeEqual Pair(
                 listOf(
                     listOf(
                         IRI.from("http://example.org/ns#beetle"),
