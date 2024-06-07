81fb53e03308def1b86f91ef60b5e2c4d4ac759c
RebekkaMa <rebekka@martens-sachsen.de>
2024-06-07 19:43:46 +0200

    Added Build native images workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,13 @@ repositories {
 dependencies {
     implementation("com.github.ajalt.clikt:clikt:4.3.0")
     implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+    implementation("io.github.microutils:kotlin-logging-jvm:4.0.0-beta-2")
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.5.6")
     implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
     implementation("org.apache.jena:jena-core:5.0.0")
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.1")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.0")
 }
 
 tasks.test {
@@ -33,4 +33,15 @@ kotlin {
 application {
     mainClass.set("MainKt")
     applicationDefaultJvmArgs = setOf("-Xss1g")
+}
+
+tasks.register<Jar>("fatJar") {
+    archiveClassifier.set("fat")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
